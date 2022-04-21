6aa69cd80b976338d73c51e68ad4de863834ebff
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-04-21 17:32:18 -0300

    🛠️ :(

--- TE2_A07 - TAD Vetor/main.c
+++ TE2_A07 - TAD Vetor/main.c
@@ -7,40 +7,31 @@ int main()
 {
     Vetor *v = vet_criar();
 
-    vet_anexar(v, 1);
-    vet_anexar(v, 2);
-    vet_anexar(v, 3);
-    vet_anexar(v, 4);
-    vet_anexar(v, 5);
-    vet_anexar(v, 6);
-    vet_anexar(v, 7);
+    vet_anexar(v, 10);
+    vet_anexar(v, 20);
+    vet_anexar(v, 30);
     vet_imprimir(v); // [10,20,30]
 
-    printf("\n");
-
-    vet_inserir(v, 15, 3);
-    vet_inserir(v, 16, 4);
-    vet_inserir(v, 17, 5);
-    vet_inserir(v, 17, 20);
+    vet_inserir(v, 15, 1);
     vet_imprimir(v); // [10,15,20,30]
 
-    // vet_inserir(v, 5, 0);
-    // vet_imprimir(v); // [5,10,15,20,30]
+    vet_inserir(v, 5, 0);
+    vet_imprimir(v); // [5,10,15,20,30]
 
-    // int elemento;
-    // if (vet_removerPosicao(v, 1, &elemento))
-    // {
-    //     printf("O elemento %d foi removido\n", elemento); // O elemento 10 foi removido
-    //     vet_imprimir(v);                                  // [5,15,20,30]
-    // }
+    int elemento;
+    if (vet_removerPosicao(v, 1, &elemento))
+    {
+        printf("O elemento %d foi removido\n", elemento); // O elemento 10 foi removido
+        vet_imprimir(v);                                  // [5,15,20,30]
+    }
 
-    // // if (vet_removerPosicao(v, 10, &elemento) == false){
-    // if (!vet_removerPosicao(v, 10, &elemento))
-    // {
-    //     printf("Remocao invalida\n");
-    // }
+    // if (vet_removerPosicao(v, 10, &elemento) == false){
+    if (!vet_removerPosicao(v, 10, &elemento))
+    {
+        printf("Remocao invalida\n");
+    }
 
-    // char str[200];
-    // vet_toString(v, str);
-    // printf("%s", str); //[5,15,20,30]
+    char str[200];
+    vet_toString(v, str);
+    printf("%s", str); //[5,15,20,30]
 }
