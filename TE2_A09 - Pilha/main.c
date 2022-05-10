6097553025e40f3864105cb2b6c5471381ff9987
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-05-10 17:50:58 -0300

    uhsaduahduahduahd

--- TE2_A09 - Pilha/main.c
+++ TE2_A09 - Pilha/main.c
@@ -4,8 +4,70 @@
 int main()
 {
 
+    // Pilha *p1 = pilha_criar();
+    // Pilha *p2;
+
+    // pilha_empilhar(p1, 10);
+    // pilha_empilhar(p1, 20);
+    // pilha_empilhar(p1, 30);
+    // pilha_empilhar(p1, 40);
+    // pilha_empilhar(p1, 50);
+
+    // pilha_imprimir(p1);
+
+    // int posic;
+
+    // pilha_topo(p1, &posic);
+
+    // printf("\n %i", posic);
+
+    // int tam;
+
+    // tam = pilha_tamanho(p1);
+
+    // printf("\n %i", tam);
+
+    // p2 = pilha_clone(p1);
+
+    // printf("\n");
+
+    // pilha_imprimir(p2);
+
+    // int desempilhado;
+
+    // pilha_desempilhar(p1, &desempilhado);
+
+    // printf("\n");
+
+    // pilha_imprimir(p1);
+
+    // printf("\nInversão da pilha:\n");
+
+    // pilha_imprimir(p1);
+
+    // pilha_inverter(p1);
+
+    // pilha_imprimir(p1);
+
+    // printf("\nEmpilhar todos:\n");
+
+    // int vet[4] = {60, 70, 80, 90};
+
+    // pilha_empilharTodos(p1, vet, 4);
+
+    // pilha_imprimir(p1);
+
+    // printf("\nPilha to string:\n");
+
+    // char str[100];
+
+    // pilha_toString(p1, &str);
+
+    // printf("%s", str);
+
+    // ------------------------------------------------------------------------
+
     Pilha *p1 = pilha_criar();
-    Pilha *p2;
 
     pilha_empilhar(p1, 10);
     pilha_empilhar(p1, 20);
@@ -15,55 +77,8 @@ int main()
 
     pilha_imprimir(p1);
 
-    int posic;
-
-    pilha_topo(p1, &posic);
-
-    printf("\n %i", posic);
-
-    int tam;
-
-    tam = pilha_tamanho(p1);
-
-    printf("\n %i", tam);
-
-    p2 = pilha_clone(p1);
-
-    printf("\n");
-
-    pilha_imprimir(p2);
-
-    int desempilhado;
-
-    pilha_desempilhar(p1, &desempilhado);
-
-    printf("\n");
-
-    pilha_imprimir(p1);
-
-    printf("\nInversão da pilha:\n");
-
-    pilha_imprimir(p1);
-
-    pilha_inverter(p1);
-
-    pilha_imprimir(p1);
-
-    printf("\nEmpilhar todos:\n");
-
-    int vet[4] = {60, 70, 80, 90};
-
-    pilha_empilharTodos(p1, vet, 4);
-
-    pilha_imprimir(p1);
-
-    printf("\nPilha to string:\n");
-
-    char str[100];
-
-    pilha_toString(p1, &str);
-
-    printf("%s", str);
+    pilha_destruir((p1));
+    printf(" opa \n\n\n\n %u", &p1);
 
     return 0;
 }
--- TE2_A09 - Pilha/pilha_encadeada.c
+++ TE2_A09 - Pilha/pilha_encadeada.c
@@ -1,30 +1,115 @@
 #include "pilha.h"
 
-#define TAM_INICIAL 5
-
 /**************************************
  * DADOS
  **************************************/
+typedef struct no
+{
+    TipoElemento dado;
+    struct no *prox;
+} No;
+
 struct pilha
 {
-    TipoElemento *vetor;
-    int tamVetor;
+    No *topo;
     int qtdeElementos;
 };
 
 /**************************************
  * IMPLEMENTAÇÃO
  **************************************/
-// Desenvolva as funções
-Pilha *pilha_criar();
-void pilha_destruir(Pilha **endereco);
-bool pilha_empilhar(Pilha *p, TipoElemento elemento);
+// Desenvolva as funçõesu
+Pilha *pilha_criar()
+{
+
+    Pilha *aux;
+    aux = (Pilha *)malloc(sizeof(Pilha));
+
+    aux->topo = NULL;
+    aux->qtdeElementos = 0;
+
+    return aux;
+}
+void pilha_destruir(Pilha **endereco)
+{
+
+    No *node = (*endereco)->topo;
+    No *aux = node;
+
+    while (aux->prox != NULL)
+    {
+        aux = aux->prox;
+        free(aux);
+    }
+
+    free((*endereco)->topo);
+    (*endereco)->topo = NULL;
+    (*endereco)->qtdeElementos = NULL;
+    free((*endereco));
+}
+bool pilha_empilhar(Pilha *p, TipoElemento elemento)
+{
+    No *node = (No *)malloc(sizeof(No));
+    if (node == NULL)
+    {
+        printf("Erro de alocação. \n");
+    }
+    else
+    {
+        node->dado = elemento;
+        node->prox = NULL;
+    }
+
+    if (p->topo == NULL)
+    {
+        p->topo = node;
+    }
+    else
+    {
+        No *aux = p->topo;
+
+        while (aux->prox != NULL)
+        {
+            aux = aux->prox;
+        }
+
+        aux->prox = node;
+    }
+
+    p->qtdeElementos++;
+
+    return true;
+}
 bool pilha_desempilhar(Pilha *p, TipoElemento *saida);
 bool pilha_topo(Pilha *p, TipoElemento *saida);
 bool pilha_vazia(Pilha *p);
-void pilha_imprimir(Pilha *p);
-int pilha_tamanho(Pilha *p);
-Pilha *pilha_clone(Pilha *p);
+void pilha_imprimir(Pilha *p)
+{
+
+    printf("hmm\n");
+
+    No *aux = p->topo;
+
+    for (int i = 0; i < p->qtdeElementos; i++)
+    {
+        printf("%i ", aux->dado);
+        aux = aux->prox;
+    }
+}
+int pilha_tamanho(Pilha *p)
+{
+    return p->qtdeElementos;
+}
+Pilha *pilha_clone(Pilha *p)
+{
+
+    Pilha *aux;
+    aux = (Pilha *)malloc(sizeof(Pilha));
+    aux->qtdeElementos = p->qtdeElementos;
+    aux->topo = p->topo;
+
+    return aux;
+}
 void pilha_inverter(Pilha *p);
 bool pilha_empilharTodos(Pilha *p, TipoElemento *vetor, int tamVetor);
 bool pilha_toString(Pilha *f, char *str);
