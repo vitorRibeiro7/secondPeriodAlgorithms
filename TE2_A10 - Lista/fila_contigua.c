8e0019565d79e8533e3c5f977fe6e8db9d680bf8
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-05-16 18:50:12 -0300

    🍁 fila coming

--- TE2_A10 - Lista/fila_contigua.c
+++ TE2_A10 - Lista/fila_contigua.c
@@ -1,4 +1,5 @@
 #include "fila.h"
+#include <string.h>
 
 #define TAM_INICIAL 5
 /**************************************
@@ -41,9 +42,67 @@ void fila_destruir(Fila **enderecoFila)
 	free(aux);
 	aux = NULL;
 }
-bool fila_inserir(Fila *f, TipoElemento elemento);
-bool fila_remover(Fila *f, TipoElemento *saida);  // estratégia do scanf
-bool fila_primeiro(Fila *f, TipoElemento *saida); // estratégia do scanf
+bool fila_inserir(Fila *f, TipoElemento elemento)
+{
+
+	if (f == NULL || elemento == NULL)
+	{
+		return false;
+	}
+
+	f->vetor[f->qtdeElementos] = elemento;
+	f->qtdeElementos++;
+
+	if (f->qtdeElementos > 0)
+	{
+		f->fim++;
+	}
+
+	return true;
+}
+bool fila_remover(Fila *f, TipoElemento *saida)
+{
+
+	int *aux = calloc(f->qtdeElementos, sizeof(int));
+
+	*saida = f->vetor[0];
+
+	for (int i = 0; i < f->qtdeElementos; i++)
+	{
+		aux[i] = f->vetor[i];
+	}
+
+	for (int i = 0; i < f->qtdeElementos; i++)
+	{
+		f->vetor[i] = 0;
+	}
+
+	int j = 1;
+
+	for (int i = 0; i < f->qtdeElementos; i++)
+	{
+		f->vetor[i] = aux[j];
+		j++;
+	}
+
+	if (f->qtdeElementos > 0)
+	{
+		f->fim--;
+	}
+
+	if (f->qtdeElementos == 0)
+	{
+		return false;
+	}
+
+	return true;
+}
+bool fila_primeiro(Fila *f, TipoElemento *saida)
+{
+	*saida = f->vetor[0];
+
+	return true;
+}
 bool fila_vazia(Fila *f)
 {
 	if (f->qtdeElementos == 0)
@@ -59,7 +118,73 @@ int fila_tamanho(Fila *f)
 {
 	return f->qtdeElementos;
 }
-void fila_imprimir(Fila *f);
-Fila *fila_clone(Fila *f);
-bool fila_toString(Fila *f, char *str);
-bool fila_inserirTodos(Fila *f, TipoElemento *vetor, int tamVetor);
+void fila_imprimir(Fila *f)
+{
+
+	for (int i = 0; i < f->tamVetor; i++)
+	{
+		printf("[");
+		printf("%i");
+		if (i < f->qtdeElementos - 1)
+		{
+			printf(", ");
+		}
+		printf("]");
+	}
+}
+Fila *fila_clone(Fila *f)
+{
+
+	Fila *aux = (Fila *)malloc(sizeof(Fila));
+	aux->vetor = (int *)calloc(f->tamVetor, sizeof(int));
+
+	for (int i = 0; i < f->qtdeElementos; i++)
+	{
+		aux->vetor[i] = f->vetor[i];
+	}
+
+	aux->inicio = f->inicio;
+	aux->fim = f->fim;
+	aux->qtdeElementos = f->qtdeElementos;
+	aux->tamVetor = f->tamVetor;
+
+	return aux;
+}
+bool fila_toString(Fila *f, char *str)
+{
+	if (f == NULL)
+	{
+		return false;
+	}
+
+	str[0] = '\0';
+
+	strcat(str, "["); // insere na string o valor passado
+
+	for (int i = 0; i < f->qtdeElementos; i++)
+	{
+		char casting[50];
+
+		sprintf(casting, "%d", f->vetor[i]);
+		strcat(str, casting);
+
+		if (i < (f->qtdeElementos) - 1)
+		{
+			strcat(str, ",");
+		}
+	}
+
+	strcat(str, "]\n");
+
+	return true;
+}
+bool fila_inserirTodos(Fila *f, TipoElemento *vetor, int tamVetor)
+{
+
+	for (int i = 0; i < tamVetor; i++)
+	{
+		fila_inserir(f, vetor[i]);
+	}
+
+	return true;
+}
--- TE2_A10 - Lista/fila_encadeada.c
+++ TE2_A10 - Lista/fila_encadeada.c
@@ -19,9 +19,56 @@ struct fila
 /**************************************
  * IMPLEMENTAÇÃO
  **************************************/
-Fila *fila_criar();
-void fila_destruir(Fila **enderecoFila);
-bool fila_inserir(Fila *f, TipoElemento elemento);
+Fila *fila_criar()
+{
+    Fila *aux = (Fila *)malloc(sizeof(Fila));
+    aux->fim = NULL;
+    aux->inicio = NULL;
+    aux->qtde = 0;
+
+    return aux;
+}
+void fila_destruir(Fila **enderecoFila)
+{
+    No *aux;
+    Fila *f = enderecoFila;
+
+    while (f->inicio != NULL)
+    {
+        aux = f->inicio;
+        f->inicio = f->inicio->prox;
+        free(aux);
+    }
+
+    free(*enderecoFila);
+
+    return true;
+}
+bool fila_inserir(Fila *f, TipoElemento elemento)
+{
+
+    if (f == NULL)
+    {
+        return false;
+    }
+
+    No *node = (No *)malloc(sizeof(No));
+    node->dado = elemento;
+    node->prox = NULL;
+
+    if (f->inicio == NULL)
+    {
+        f->inicio = node;
+        f->fim = node;
+    }
+    else
+    {
+        f->fim->prox = node;
+        f->fim = node;
+    }
+
+    return true;
+}
 bool fila_remover(Fila *f, TipoElemento *saida);  // estratégia do scanf
 bool fila_primeiro(Fila *f, TipoElemento *saida); // estratégia do scanf
 bool fila_vazia(Fila *f);
