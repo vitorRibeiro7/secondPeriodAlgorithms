24cd3a3d28e3ce3ec279cbf0ea5383f56bbdfe46
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-05-18 16:36:45 -0300

    🦷 oi

--- TE2_A10 - Lista/fila_encadeada.c
+++ TE2_A10 - Lista/fila_encadeada.c
@@ -61,19 +61,121 @@ bool fila_inserir(Fila *f, TipoElemento elemento)
         f->inicio = node;
         f->fim = node;
     }
+
+    f->fim->prox = node;
+    f->fim = node;
+
+    f->qtde++;
+
+    return true;
+}
+bool fila_remover(Fila *f, TipoElemento *saida)
+{
+
+    *saida = f->inicio->dado;
+
+    No *aux = f->inicio;
+
+    f->inicio = f->inicio->prox;
+
+    free(aux);
+
+    f->qtde--;
+
+    return true;
+
+} // estratégia do scanf
+bool fila_primeiro(Fila *f, TipoElemento *saida)
+{
+    *saida = f->inicio->dado;
+} // estratégia do scanf
+bool fila_vazia(Fila *f)
+{
+    if (f->qtde == 0)
+    {
+        return true;
+    }
     else
     {
-        f->fim->prox = node;
-        f->fim = node;
+        return false;
+    }
+}
+int fila_tamanho(Fila *f)
+{
+    return f->qtde;
+}
+void fila_imprimir(Fila *f)
+{
+
+    No *aux = f->inicio;
+
+    printf("[");
+
+    for (int i = 0; i < f->qtde; i++)
+    {
+        printf("%i", aux->dado);
+        aux = aux->prox;
+
+        if (i < f->qtde - 1)
+        {
+            printf(",");
+        }
+    }
+
+    printf("]");
+}
+Fila *fila_clone(Fila *f)
+{
+    Fila *clone;
+    clone = fila_criar;
+
+    No *aux = f->inicio;
+
+    for (int i = 0; i < f->qtde; i++)
+    {
+        fila_inserir(clone, aux->dado);
+        aux = aux->prox;
+    }
+}
+bool fila_toString(Fila *f, char *str)
+{
+    if (f == NULL)
+    {
+        return false;
+    }
+
+    No *aux = f->inicio;
+
+    str[0] = '\0';
+
+    strcat(str, "["); // insere na string o valor passado
+
+    for (int i = 0; i < f->qtde; i++)
+    {
+        char casting[50];
+
+        sprintf(casting, "%d", aux->dado);
+        strcat(str, casting);
+
+        if (i < (f->qtde) - 1)
+        {
+            strcat(str, ",");
+        }
+
+        aux = aux->prox;
+    }
+
+    strcat(str, "]\n");
+
+    return true;
+}
+bool fila_inserirTodos(Fila *f, TipoElemento *vetor, int tamVetor)
+{
+
+    for (int i = 0; i < tamVetor; i++)
+    {
+        fila_inserir(f, vetor[i]);
     }
 
     return true;
 }
-bool fila_remover(Fila *f, TipoElemento *saida);  // estratégia do scanf
-bool fila_primeiro(Fila *f, TipoElemento *saida); // estratégia do scanf
-bool fila_vazia(Fila *f);
-int fila_tamanho(Fila *f);
-void fila_imprimir(Fila *f);
-Fila *fila_clone(Fila *f);
-bool fila_toString(Fila *f, char *str);
-bool fila_inserirTodos(Fila *f, TipoElemento *vetor, int tamVetor);
--- TE2_A10 - Lista/main.c
+++ TE2_A10 - Lista/main.c
@@ -13,56 +13,65 @@ int main()
     /**************************************
      * Teste insercao e primeiro
      **************************************/
-    Fila *f1 = fila_criar();
-    fila_inserir(f1, 10);
-    fila_inserir(f1, 20);
-    fila_inserir(f1, 30);
-    fila_inserir(f1, 30);
-    fila_inserir(f1, 30);
-    fila_inserir(f1, 30);
-    fila_inserir(f1, 30);
-    fila_toString(f1, &str);
-    printf("%s\n", str);
+    // Fila *f1 = fila_criar();
+    // fila_inserir(f1, 10);
+    // fila_inserir(f1, 20);
+    // fila_inserir(f1, 30);
+    // fila_inserir(f1, 40);
+    // fila_inserir(f1, 50);
+    // fila_inserir(f1, 60);
+    // fila_inserir(f1, 70);
+    // fila_inserir(f1, 80);
 
-    fila_primeiro(f1, &elemento);
-    printf("%i\n", elemento);
+    // fila_imprimir(f1);
+    // fila_toString(f1, &str);
+    // printf("%s\n", str);
 
-    /**************************************
-     * Teste remocao
-     **************************************/
-    fila_remover(f1, &elemento);
+    // fila_primeiro(f1, &elemento);
+    // printf("%i\n", elemento);
 
-    fila_remover(f1, &elemento);
+    // printf("\n");
+    // printf("\n");
+    // printf("\n");
 
-    fila_remover(f1, &elemento);
+    // /**************************************
+    //  * Teste remocao
+    //  **************************************/
+    // fila_remover(f1, &elemento);
 
-    bool resultado = fila_remover(f1, &elemento);
-    fila_toString(f1, &str);
-    printf("%s\n", str);
+    // fila_remover(f1, &elemento);
 
-    //     fila_destruir(&f1);
+    // fila_remover(f1, &elemento);
+
+    // fila_toString(f1, &str);
+    // printf("%s\n", str);
+
+    // fila_destruir(&f1);
 
     //     /**************************************
     //      * Teste inserir todos e clone
     //      **************************************/
-    //     int vetor[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    //     f1 = fila_criar();
-    //     fila_inserirTodos(f1, vetor, 12);
+    int vetor[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    Fila *f1 = fila_criar();
+    fila_inserirTodos(f1, vetor, 12);
 
-    //     fila_toString(f1, &str);
+    fila_imprimir(f1);
+    fila_toString(f1, &str);
 
-    //     Fila *f2 = fila_clone(f1);
-    //     fila_remover(f2, &elemento);
-    //     fila_remover(f2, &elemento);
-    //     fila_remover(f2, &elemento);
-    //     fila_inserir(f2, 20);
-    //     fila_inserir(f2, 21);
-    //     fila_inserir(f2, 22);
+    printf("%s", str);
 
-    //     fila_toString(f2, &str);
+    Fila *f2 = fila_clone(f1);
+    fila_imprimir(f2);
 
-    //     fila_destruir(&f1);
-    //     fila_destruir(&f2);
+    // fila_remover(f2, &elemento);
+    // fila_remover(f2, &elemento);
+    // fila_remover(f2, &elemento);
+    // fila_inserir(f2, 20);
+    // fila_inserir(f2, 21);
+    // fila_inserir(f2, 22);
 
-    //     printf("%s", str);
+    // fila_toString(f2, &str);
+
+    // fila_destruir(&f1);
+    // fila_destruir(&f2);
 }
