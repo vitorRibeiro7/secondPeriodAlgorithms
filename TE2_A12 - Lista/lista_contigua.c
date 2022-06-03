787c123e837d635153c3b9d2934cdcc705903c85
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-06-03 03:27:16 -0300

    🚀 tamanho da lista e verificação vazia.

--- TE2_A12 - Lista/lista_contigua.c
+++ TE2_A12 - Lista/lista_contigua.c
@@ -195,8 +195,27 @@ bool lista_buscar(Lista *l, int posicao, TipoElemento *endereco)
 	return true;
 }
 
-int lista_tamanho(Lista *l);
-bool lista_vazia(Lista *l);
+int lista_tamanho(Lista *l)
+{
+	return l->qtde;
+}
+
+bool lista_vazia(Lista *l)
+{
+	if (l->vetor == NULL)
+	{
+		return false;
+	}
+
+	if (l->qtde == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 bool lista_toString(Lista *l, char *str);
 
 /**************************************
