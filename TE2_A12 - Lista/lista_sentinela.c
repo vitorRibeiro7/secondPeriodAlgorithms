8e62cce08767fdd2793c7a48b86c4183a72d2b32
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-06-13 01:46:06 -0300

    :rocket: Criar, Destruir e Anexar estão prontos.

--- TE2_A12 - Lista/lista_sentinela.c
+++ TE2_A12 - Lista/lista_sentinela.c
@@ -21,10 +21,69 @@ typedef struct no
  * IMPLEMENTAÇÃO
  **************************************/
 
-Lista *lista_criar();
-void lista_destruir(Lista **endereco);
+Lista *lista_criar()
+{
 
-bool lista_anexar(Lista *l, TipoElemento elemento);
+    Lista *new = (Lista *)malloc(sizeof(Lista));
+    new->qtde = 0;
+    No *sent = (No *)malloc(sizeof(No));
+    new->sentinela = sent;
+    sent->ant = sent;
+    sent->prox = sent;
+
+    return new;
+}
+void lista_destruir(Lista **endereco)
+{
+    No *lista = (*endereco);
+    No *aux = (*endereco)->sentinela;
+    No *aux2 = (*endereco)->sentinela;
+    aux = aux->prox;
+
+    while (aux != (*endereco)->sentinela)
+    {
+        free(aux);
+        aux = aux->prox;
+    }
+
+    free(aux2);
+    free(lista);
+}
+
+bool lista_anexar(Lista *l, TipoElemento elemento)
+{
+
+    if (l == NULL)
+    {
+        return true;
+    }
+
+    No *node = (No *)malloc(sizeof(No));
+    node->ant = NULL;
+    node->prox = NULL;
+    node->dado = elemento;
+
+    No *sent = l->sentinela;
+
+    if (l->qtde == 0)
+    {
+        sent->prox = node;
+        sent->ant = node;
+        node->prox = sent;
+        node->ant = sent;
+    }
+    else
+    {
+        sent->ant->prox = node;
+        node->ant = sent->ant;
+        sent->ant = node;
+        node->prox = sent;
+    }
+
+    l->qtde++;
+
+    return true;
+}
 bool lista_inserir(Lista *l, TipoElemento elemento, int posicao);
 
 bool lista_removerPosicao(Lista *l, int posicao, TipoElemento *endereco);
@@ -41,4 +100,25 @@ bool lista_toString(Lista *l, char *str);
 /**************************************
  * AUXILIARES
  **************************************/
-void lista_imprimir(Lista *l);
+void lista_imprimir(Lista *l)
+{
+
+    No *aux = l->sentinela;
+    aux = aux->prox;
+
+    printf("[");
+
+    for (int i = 0; i < l->qtde; i++)
+    {
+        printf("%i", aux->dado);
+
+        if (i < l->qtde - 1)
+        {
+            printf(", ");
+        }
+
+        aux = aux->prox;
+    }
+
+    printf("]");
+}
--- TE2_A12 - Lista/main.c
+++ TE2_A12 - Lista/main.c
@@ -11,75 +11,75 @@ int main()
         lista_anexar(l, 10);
         lista_anexar(l, 20);
         lista_anexar(l, 40);
-        lista_inserir(l, 30, 2);
+        // lista_inserir(l, 30, 2);
         lista_anexar(l, 100);
         lista_anexar(l, 60);
         lista_anexar(l, 80);
-        lista_inserir(l, 70, 6);
+        // lista_inserir(l, 70, 6);
         lista_imprimir(l);
-        printf(" substituido valor na posicao");
-        printf("\n");
+        // printf(" substituido valor na posicao");
+        // printf("\n");
 
-        lista_substituir(l, 4, 50);
-        lista_imprimir(l);
-        printf(" ----------------------------'");
-        printf("\n");
-        printf("\n");
+        // lista_substituir(l, 4, 50);
+        // lista_imprimir(l);
+        // printf(" ----------------------------'");
+        // printf("\n");
+        // printf("\n");
 
-        int elemento = 60; // ELEMENTO A SER PROCURADO
-        int pos;
-        pos = lista_posicao(l, elemento);
-        if (pos != -1)
-        {
-                printf("Elemento %i encontrado na posicao %i (busca a posicao do elemento)", elemento, pos);
-        }
-        else
-        {
-                printf("Elemento não encontrado.");
-        }
+        // int elemento = 60; // ELEMENTO A SER PROCURADO
+        // int pos;
+        // pos = lista_posicao(l, elemento);
+        // if (pos != -1)
+        // {
+        //         printf("Elemento %i encontrado na posicao %i (busca a posicao do elemento)", elemento, pos);
+        // }
+        // else
+        // {
+        //         printf("Elemento não encontrado.");
+        // }
 
-        printf("\n");
-        printf("\n");
+        // printf("\n");
+        // printf("\n");
 
-        int elem;
-        int posic = 3; // POSICAO A SER EXTRAIDA DO DADO
-        lista_buscar(l, posic, &elem);
-        printf("Elemento %i encontrado na posicao %i (busca o elemento na posicao)", elem, posic);
+        // int elem;
+        // int posic = 3; // POSICAO A SER EXTRAIDA DO DADO
+        // lista_buscar(l, posic, &elem);
+        // printf("Elemento %i encontrado na posicao %i (busca o elemento na posicao)", elem, posic);
 
-        printf("\n");
+        // printf("\n");
 
-        int removido;
-        if (lista_removerPosicao(l, 0, &removido))
-        {
-                lista_imprimir(l);
-                printf("removido: %i", removido);
-        }
-        printf("\n");
-        if (lista_removerPosicao(l, 6, &removido))
-        {
-                lista_imprimir(l);
-                printf("removido: %i", removido);
-        }
-        printf("\n");
+        // int removido;
+        // if (lista_removerPosicao(l, 0, &removido))
+        // {
+        //         lista_imprimir(l);
+        //         printf("removido: %i", removido);
+        // }
+        // printf("\n");
+        // if (lista_removerPosicao(l, 6, &removido))
+        // {
+        //         lista_imprimir(l);
+        //         printf("removido: %i", removido);
+        // }
+        // printf("\n");
 
-        int retorno;
-        retorno = lista_removerElemento(l, 40);
-        if (retorno == 0)
-        {
-                printf("\nO elemento não existe!");
-        }
-        else
-        {
-                printf("\nElemento removido!");
-        }
+        // int retorno;
+        // retorno = lista_removerElemento(l, 40);
+        // if (retorno == 0)
+        // {
+        //         printf("\nO elemento não existe!");
+        // }
+        // else
+        // {
+        //         printf("\nElemento removido!");
+        // }
 
-        printf("\n");
-        lista_imprimir(l);
+        // printf("\n");
+        // lista_imprimir(l);
 
-        printf("\n\nTo string!\n");
-        char str[] = "\n";
-        lista_toString(l, &str);
-        printf("%s", str);
+        // printf("\n\nTo string!\n");
+        // char str[] = "\n";
+        // lista_toString(l, &str);
+        // printf("%s", str);
 
         return 0;
 }
